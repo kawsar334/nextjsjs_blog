14ef267a098d46676c103bfb91dbb4e5daf35dbd
kawsar firoz <kawsarfiroz11@gmail.com>
2025-01-23 20:18:05 +0300

    updated env code

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-
-     env: {
-    KINDE_CLIENT_ID: process.env.KINDE_CLIENT_ID,
-    KINDE_CLIENT_SECRET: process.env.KINDE_CLIENT_SECRET,
-    KINDE_ISSUER_URL: process.env.KINDE_ISSUER_URL,
-    KINDE_SITE_URL: process.env.KINDE_SITE_URL,
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
