e3718bd1c4928648385249383be2a6a9fb177578
kawsar firoz <kawsarfiroz11@gmail.com>
2025-01-23 19:15:48 +0300

    modified blog page

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,22 +1,9 @@
-import Blog from "@/components/Blog";
+import React from 'react'
 
-import Header from '../components/Header';
-
-
-export default async function BlogPage() {
-    const res = await fetch('https://jsonplaceholder.typicode.com/posts');
-    const posts = await res.json();
-    return (
-        <div>
-            <h1 className="text-3xl text-center font-bold">Blog Page</h1>
-            <Header />
-
-            <ul className="flex justify-center items-center flex-wrap  gap-2">
-                {posts.map((post) => (
-
-                    <Blog post={post} />
-                ))}
-            </ul>
-        </div>
-    );
+const Blogpage = () => {
+  return (
+    <div>Blogpage</div>
+  )
 }
+
+export default Blogpage
